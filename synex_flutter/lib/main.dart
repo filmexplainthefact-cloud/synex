7dde61dc506c42297bdfd9bf6eaaa82c94ebedc9
filmexplainthefact-cloud <filmexplainthefact@gmail.com>
2026-04-15 15:13:59 +0530

    Update main.dart

--- synex_flutter/lib/main.dart
+++ synex_flutter/lib/main.dart
@@ -18,7 +18,7 @@ void main() async {
   SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
     statusBarColor: Colors.transparent,
     statusBarIconBrightness: Brightness.light,
-    navigationBarIconBrightness: Brightness.light,
+   // navigationBarIconBrightness: Brightness.light,
   ));
 
   await Firebase.initializeApp(
